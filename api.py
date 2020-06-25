5be7e24b3690918796272536d071b8f4ff863c71
jakmat <jakubmatusiak87@gmail.com>
2020-06-25 22:36:58 +0200

    Separate api repo

---
+++ api.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, jsonify
+from main import perform_observation
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/api/v1/ephemerides', methods=['POST'])
+def planets_all():
+    object = request.args.get('object')
+    timestamp = request.args.get('timestamp')
+    observable = perform_observation(object, timestamp)
+    observation = jsonify(observable)
+    print(observation)
+    return observation
+
+app.run()
