0c5f4e605a47af1d0976013a3f564f47f1b79a28
jakmat <jakubmatusiak87@gmail.com>
2020-09-10 19:02:01 +0200

    * [changed] Test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def dummy_api():
-    return jsonify(data="This is a dummy API!")
+    return jsonify(data="This is a test")
 
 if __name__ == "__main__":
     app.run()
