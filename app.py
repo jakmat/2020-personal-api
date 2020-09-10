d332a77fbd014f0252df558b02fc866983ec021e
jakmat <jakubmatusiak87@gmail.com>
2020-09-10 20:57:53 +0200

    test

--- app.py
+++ app.py
@@ -1,23 +1,22 @@
-from flask import Flask, jsonify
-# from flask import Flask, request, jsonify
-# from main import perform_observation
-# from flask_cors import CORS
+from flask import Flask, request, jsonify
+from main import perform_observation
+from flask_cors import CORS
 
 app = Flask(__name__)
-# CORS(app)
+CORS(app)
 
-@app.route('/')
+@app.route('/test')
 def test_api():
     return jsonify(data="This is a test")
 
-# @app.route('/celestial-objects', methods=['POST'])
-# def planets_all():
-#     object = request.args.get('object')
-#     timestamp = request.args.get('timestamp')
-#     observable = perform_observation(object, timestamp)
-#     observation = jsonify(observable)
-#     print(observation)
-#     return observation
+@app.route('/celestial-objects', methods=['POST'])
+def planets_all():
+    object = request.args.get('object')
+    timestamp = request.args.get('timestamp')
+    observable = perform_observation(object, timestamp)
+    observation = jsonify(observable)
+    print(observation)
+    return observation
 
 if __name__ == "__main__":
     app.run()
